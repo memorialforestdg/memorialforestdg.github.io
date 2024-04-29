6280c43b85a3ff356ff77e1704faea3e4ee4337d
bnjmnrsh <bnjmnrsh@gmail.com>
2024-04-29 13:37:33 +0100

    Configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+// Compatibility settings for StandardJS + Prettier
+module.exports = {
+  editorconfig: 'true',
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // Robots
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from 'astro-robots-txt'
 
 // Icons
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // Sitemap
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [robotsTxt(), icon(), sitemap()]
-});
+})
