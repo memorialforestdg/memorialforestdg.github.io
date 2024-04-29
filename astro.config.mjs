d050cf7829afe8b839aab7f3bbacf9b4cbd9482b
bnjmnrsh <bnjmnrsh@gmail.com>
2024-04-29 13:32:08 +0100

    Configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+// Robots
+import robotsTxt from "astro-robots-txt";
+
+// Icons
+import icon from "astro-icon";
+
+// Sitemap
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [robotsTxt(), icon(), sitemap()]
+});
