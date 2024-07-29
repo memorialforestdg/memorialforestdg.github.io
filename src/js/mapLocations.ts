7c9736f20000d59d87163c873f20bee2162874c1
Benjamin Rush <bnjmnrsh@gmail.com>
2024-07-29 22:41:44 +0100

    Update site micro discriptions.

--- src/js/mapLocations.ts
+++ src/js/mapLocations.ts
@@ -24,7 +24,7 @@ export const mapLocations = [
   {
     id: 'castle-douglas',
     content:
-      'The Dispersed Memorial Forest:<br/> <a href="./castle-douglas.com">Castle Douglas</a>',
+      'The Dispersed Memorial Forest:<br/> <a href="./castle-douglas.com">Castle Douglas</a> at Threave Nature Reserve.',
     popup: {offset: {x: 30, y: -22}},
     lat: 54.933848,
     lon: -3.959061,
@@ -34,7 +34,7 @@ export const mapLocations = [
   {
     id: 'dumfries',
     content:
-      'The Dispersed Memorial Forest:<br/> <a href="./dumfries">Dumfries</a>',
+      'The Dispersed Memorial Forest:<br/> <a href="./dumfries">Dumfries</a>, a XXkm walking trail between Mountainhall Three Roads End and Bankend Road, on the Crichton Estate.',
     popup: {offset: {x: 30, y: -22}},
     lat: 55.057851,
     lon: -3.601577,
@@ -44,30 +44,30 @@ export const mapLocations = [
   {
     id: 'moffat',
     content:
-      'The Dispersed Memorial Forest:<br/> <a href="./moffat">Moffat</a>',
+      'The Dispersed Memorial Forest:<br/> <a href="./moffat">Moffat</a> at the Moffat Community Nature Reserve.',
     popup: {offset: {x: 30, y: -22}},
-    lat: 55.3332407,
-    lon: -3.444357,
+    lat: 55.319197,
+    lon: -3.43987,
     open: false,
     icon: iconOpts
   },
   {
     id: 'sanquhar',
     content:
-      'The Dispersed Memorial Forest:<br/> <a href="./sanquhar">Sanquhar</a>',
+      'The Dispersed Memorial Forest:<br/> <a href="./sanquhar">Sanquhar</a>, near the junction of Bell Crescent and Deer Park Avenue.',
     popup: {offset: {x: 30, y: -22}},
-    lat: 55.3676858,
-    lon: -3.9236766,
+    lat: 55.363505,
+    lon: -3.923666,
     open: false,
     icon: iconOpts
   },
   {
     id: 'stranraer',
     content:
-      'The Dispersed Memorial Forest:<br/> <a href="./stranraer">Stranraer</a>',
+      'The Dispersed Memorial Forest:<br/> <a href="./stranraer">Stranraer</a> is found within the Community Garden, behind the Galloway Community Hospital car park.',
     popup: {offset: {x: 30, y: -22}},
-    lat: 54.9044332,
-    lon: -5.026204,
+    lat: 54.89962,
+    lon: -5.020083,
     open: false,
     icon: iconOpts
   }
