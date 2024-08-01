74027e9c62c508640c2543218da626895fc0df99
Benjamin Rush <bnjmnrsh@gmail.com>
2024-08-01 14:58:27 +0100

    Add purge-css to remove unused tachyons styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,14 +15,22 @@ import icon from 'astro-icon';
 
 import relativeLinks from "astro-relative-links";
 import mdx from "@astrojs/mdx";
-
 import playformCompress from "@playform/compress";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'auto',
+  },
   // site: 'https://example.com', // We are not setting this as we want to use relative links, and deploy to mutiple domains eg yourname.github.io and yourname.com.
-  integrations: [icon(), relativeLinks(), mdx() ],
-  // playformCompress(), robotsTxt(), sitemap(), // prebuild()
+  integrations: [icon(), relativeLinks(), purgecss({
+    fontFace: true,
+    variables: true,
+    rejectedCss: true,
+    content: ["src/**/*.{astro,md,mdx,html}"],
+  })],
+  // mdx(),  robotsTxt(), sitemap(), prebuild(), playformCompress(),
   output: 'static',
   experimental: {
     env: {
