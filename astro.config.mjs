32b8604df3fe83f6acb5f78f1f7b3b56920b98ec
Benjamin Rush <bnjmnrsh@gmail.com>
2024-08-05 12:20:52 +0100

    Remove cruft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,14 @@
 import { defineConfig, envField } from "astro/config";
-import prebuild from './src/intergrations/prebuild';
 
-// Icons
 import icon from 'astro-icon';
-
-// Robots
-// import robotsTxt from 'astro-robots-txt';
-
-// Sitemap
-// import sitemap from '@astrojs/sitemap';
-
-// Better Images
-// import { imageService } from "@unpic/astro/service";
-
-import relativeLinks from "astro-relative-links";
-import mdx from "@astrojs/mdx";
-import playformCompress from "@playform/compress";
 import purgecss from "astro-purgecss";
 
-// https://astro.build/config
 export default defineConfig({
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: 'auto'
   },
-  // site: 'https://example.com', // We are not setting this as we want to use relative links, and deploy to mutiple domains eg yourname.github.io and yourname.com.
-  integrations: [icon(), relativeLinks()],
-  // mdx(),  robotsTxt(), sitemap(), prebuild(), playformCompress(), purgecss()
+  // site: 'https://example.com', // We are not setting this as we want to deploy to domain mirrors, e.g. yourname.github.io and yourname.com.
+  integrations: [icon(), purgecss()],
   output: 'static',
   experimental: {
     env: {
