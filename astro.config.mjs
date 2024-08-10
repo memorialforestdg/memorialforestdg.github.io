65c91b6ecd27472a1a62dea33a258fcf1b73f2fb
Benjamin Rush <bnjmnrsh@gmail.com>
2024-08-10 13:52:29 +0100

    PurgeCSS config to its own file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,16 @@
 import { defineConfig, envField } from "astro/config";
-
 import icon from 'astro-icon';
+import { shield } from '@kindspells/astro-shield'
+import relativeLinks from 'astro-relative-links';
 import purgecss from "astro-purgecss";
+import purgeOpts from './purgecss.config.mjs'
 
 export default defineConfig({
   build: {
     inlineStylesheets: 'auto'
   },
   // site: 'https://example.com', // We are not setting this as we want to deploy to domain mirrors, e.g. yourname.github.io and yourname.com.
-  integrations: [icon(), purgecss({
-      fontFace: true,
-      variables: true,
-      keyframes: false, // needed false for transitions
-      safelist: {
-        // purgecss falsely purges some css
-        standard: [
-          // https://github.com/FullHuman/purgecss/issues/1153
-          /:hover/,
-          // https://github.com/FullHuman/purgecss/issues/978
-          /:where/,
-          /:is/,
-          // https://github.com/FullHuman/purgecss/issues/1197
-          /:not/,
-          // https://github.com/FullHuman/purgecss/issues/1215
-          /:has/,
-        ],
-        greedy: [/*astro*/], // needed for transitions
-      },
-      content: [
-        process.cwd() + '/src/**/*.{astro,html}' // Watching .astro and .html file extensions.
-      ],
-    })
-  ],
+  integrations: [icon(), relativeLinks(), shield({}), purgecss(purgeOpts) ],
   output: 'static',
   experimental: {
     env: {
---
+++ purgecss.config.mjs
@@ -0,0 +1,23 @@
+export default {
+      fontFace: true,
+      variables: true,
+      keyframes: false, // needed false for transitions
+      safelist: {
+        // purgecss falsely purges some css
+        standard: [
+          // https://github.com/FullHuman/purgecss/issues/1153
+          /:hover/,
+          // https://github.com/FullHuman/purgecss/issues/978
+          /:where/,
+          /:is/,
+          // https://github.com/FullHuman/purgecss/issues/1197
+          /:not/,
+          // https://github.com/FullHuman/purgecss/issues/1215
+          /:has/,
+        ],
+        greedy: [/*astro*/], // needed for transitions
+      },
+      content: [
+        process.cwd() + '/src/**/*.{astro,html,css}'
+      ],
+    }
