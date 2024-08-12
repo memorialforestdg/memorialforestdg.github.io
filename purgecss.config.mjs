d2d2e424c2de07eccff99fada09985d37690bfea
Benjamin Rush <bnjmnrsh@gmail.com>
2024-08-12 16:22:14 +0100

    fix(app): #11 Narrowing down purgecss config to retain leaflet styles

--- purgecss.config.mjs
+++ purgecss.config.mjs
@@ -1,12 +1,12 @@
 export default {
       fontFace: true,
       variables: true,
-      keyframes: false, // needed false for transitions
-      whitelistPatterns: [/leaflet$/],
-      whitelistPatternsChildren: [/leaflet$/],
+      keyframes: false, // false for astro transitions
       safelist: {
         // purgecss falsely purges some css
         standard: [
+          /leaflet/, // Match all classes containing 'leaflet'
+          /marker/,  // Match all classes containing 'marker'
           // https://github.com/FullHuman/purgecss/issues/1153
           /:hover/,
           // https://github.com/FullHuman/purgecss/issues/978
@@ -17,10 +17,13 @@ export default {
           // https://github.com/FullHuman/purgecss/issues/1215
           /:has/,
         ],
-        greedy: [/*astro*/], // needed for transitions
+        deep: [/leaflet/, /marker/], // Ensure nested selectors are also matched
+        greedy: [/*astro*/], // for astro transitions
       },
       content: [
         process.cwd() + '/src/**/*.{astro,html,js,ts,md,mdx}',
         process.cwd() + '/src/styles/**/*.{css}',
+        // process.cwd() + '/node_modules/leaflet/dist/leaflet.css',
       ],
+      // css: [], // Don't specify your source css files here as they will be distructively 'purged'.
     }
