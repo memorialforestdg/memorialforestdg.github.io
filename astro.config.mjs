aee8eafdf4a8c8f51077eb88614b0d0d91091b4c
Benjamin Rush <bnjmnrsh@gmail.com>
2024-10-10 00:21:47 +0100

    chore(app): Add astero-sheild
    Add security astero-sheild for Content-Security-Policy headers.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import relativeLinks from 'astro-relative-links';
 import purgecss from "astro-purgecss";
 import purgeOpts from './purgecss.config.mjs'
 import { getCurrentNonce } from "./src/js/getCurrentNonce";
+import { shield } from '@kindspells/astro-shield'
 // import removeOriginalImages from "./src/intergrations/removeOriginalImages"; // removeOriginalImages
 // import prebuild from "./src/intergrations/prebuild"; //prebuild()
 
@@ -12,7 +13,7 @@ export default defineConfig({
     inlineStylesheets: 'auto'
   },
   // site: 'https://example.com', // We are not setting this as we want to deploy to domain mirrors, e.g. yourname.github.io and yourname.com.
-  integrations: [icon(), relativeLinks(), purgecss(purgeOpts), (await import("@playform/compress")).default()],
+  integrations: [shield({}), icon(), relativeLinks(), purgecss(purgeOpts), (await import("@playform/compress")).default()],
   output: 'static',
   experimental: {
     env: {
