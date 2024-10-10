0e32a0fe1ec47f27d0813d8c6e85b06bab2dd31d
Benjamin Rush <bnjmnrsh@gmail.com>
2024-10-10 21:20:22 +0100

    build(server): Further testing of @playform/compress
    Disable CSS entirely

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,8 +13,10 @@ export default defineConfig({
   // site: 'https://example.com', // We are not setting this as we want to deploy to domain mirrors, e.g. yourname.github.io and yourname.com.
   // Using @playform/compress for general compression (images html etc.), but useing prugecss first for CSS purge & minification.
   integrations: [icon(), relativeLinks(), purgecss(purgeOpts), (await import("@playform/compress")).default(
-    {CSS:
-      { restructure: false }}
+    {
+      CSS: false,
+      Image: false
+    }
   )],
   output: 'static',
   experimental: {
